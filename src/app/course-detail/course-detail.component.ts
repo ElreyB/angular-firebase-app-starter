6e667cb68d18ea20d440c86278a801799a6a2593
elrey belmonti <elreybelmonti@elreys-MacBook.local>
2018-02-22 10:56:29 -0800

    setup master route to details

---
+++ src/app/course-detail/course-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CourseDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
